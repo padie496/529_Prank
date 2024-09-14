229f7fe214a98a858c04f89f7f1de943fe3ad8c9
padie <padie496@gmail.com>
2024-09-14 14:55:48 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        . # # # #
+        . . . . #
+        # # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        # # # # .
+        . . . . #
+        . # # # #
+        # . . . .
+        # # # # #
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        . # # # #
+        . . . . #
+        # # # # .
+        `)
+    basic.pause(500)
+})
